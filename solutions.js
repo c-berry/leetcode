a90ab1dd2bb49e20b8175ff56d17765a893bdae3
Christopher Berry <christopher.m.berry01@gmail.com>
2022-09-28 21:25:32 -0500

    complete first leetcode challenge

---
+++ solutions.js
@@ -0,0 +1,31 @@
+// These are my Leetcode solutions using javascript:
+
+// 876. Middle of the Linked List
+// Given the head of a singly linked list, return the middle node of the linked list.
+// If there are two middle nodes, return the second middle node.
+
+// This method only works if an array is provided, however, since the exercise uses linked lists, this solution is invalid:
+var middleNode = function(head) {
+    let arr = Array.from(head);
+    let headStr = arr.join("");
+    let mid = arr.length / 2;
+    return headStr.substring(mid, arr.length).split("");
+};
+// console.log(middleNode([1,2,3,4,5,6]));
+
+
+// This solution uses one slow pointer (.next) and one fast pointer (.next.next) to move thru the list returning the slow pointer when the fast pointer reaches the end of the list giving you the middle node:
+var middleNode2 = function (head) {
+    let pointerSlow = head;
+    let pointerFast = head;
+
+    while (pointerFast && pointerFast.next) {
+        pointerSlow = pointerSlow.next;
+        pointerFast = pointerFast.next.next;
+    }
+
+    return pointerSlow;
+};
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
