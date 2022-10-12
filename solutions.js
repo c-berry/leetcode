aba98e528de9a71a8d27982c0212ee4b626d3378
Christopher Berry <christopher.m.berry01@gmail.com>
2022-10-12 14:04:55 -0500

    complete leetcode problem usiing trim and split

--- solutions.js
+++ solutions.js
@@ -125,3 +125,13 @@ var strStr2 = function(haystack, needle) {
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+// A word is a maximal substring consisting of non-space characters only.
+var lengthOfLastWord = function(s) {
+    return s.trim().split(' ').at(-1).length;
+};
+// console.log(lengthOfLastWord("luffy is still joyboy"));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
