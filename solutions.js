08e5e10909c3021cd88aefc25c59c8dfd28fb611
Christopher Berry <christopher.m.berry01@gmail.com>
2022-10-19 12:40:51 -0500

    complete daily kata

--- solutions.js
+++ solutions.js
@@ -156,3 +156,33 @@ var longestCommonPrefix = function (strs) {
 
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+// An input string is valid if:
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+var isValid = function(s) {
+    if (s.length % 2 !== 0 ) return false;
+
+    var stack = [];
+    for (let c of s) {
+        if (c === ')' && stack[stack.length -1] === '('){
+            stack.pop()
+        } else if (c === '}' && stack[stack.length -1] === '{'){
+            stack.pop()
+        } else if (c === ']' && stack[stack.length -1] === '['){
+            stack.pop()
+        } else {
+            stack.push(c)
+        }
+
+    }
+
+    return !stack.length
+};
+// console.log(isValid("()"));
+// console.log(isValid("()[]{}"));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
