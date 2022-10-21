7dcb647288769182a20a6795095206fa8e00bf2e
Christopher Berry <christopher.m.berry01@gmail.com>
2022-10-21 13:33:20 -0500

    complete leetcode using solution checking if an array of nums contains duplicates

--- solutions.js
+++ solutions.js
@@ -210,3 +210,21 @@ function solution(n) {
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+var containsDuplicate = function(nums) {
+    let arr = [];
+    for(let i = 0; i < nums.length; i++) {
+        if (!arr.includes(nums[i])) {
+            arr.push(nums[i]);
+        } else {
+            return true;
+        }
+    }
+    return false;
+};
+// console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]));
+// console.log(containsDuplicate([1,2,3,4]));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
