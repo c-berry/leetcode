50e7ab0f83f57152c83a7762bf3149c9254e54ce
Christopher Berry <christopher.m.berry01@gmail.com>
2022-10-24 16:19:03 -0500

    complete daily algorithm using array methods

--- solutions.js
+++ solutions.js
@@ -228,3 +228,18 @@ var containsDuplicate = function(nums) {
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
+// Given an integer x, return true if x is palindrome integer.
+// An integer is a palindrome when it reads the same backward as forward.
+// For example, 121 is a palindrome while 123 is not.
+
+var isPalindrome = function(x) {
+    let arr = [];
+    arr.push(x);
+    return arr.join('').split('').reverse().join('') === arr.join();
+};
+// console.log(isPalindrome(121));
+// console.log(isPalindrome(122));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
