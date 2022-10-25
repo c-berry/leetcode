bfcde88eae308049547e1e8c067d92a7b06a7c29
Christopher Berry <christopher.m.berry01@gmail.com>
2022-10-25 18:19:43 -0500

    complete daily algorithm

--- solutions.js
+++ solutions.js
@@ -243,3 +243,25 @@ var isPalindrome = function(x) {
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+var twoSum = function(nums, target) {
+    var map = {};
+    for(var i = 0 ; i < nums.length ; i++){
+        var n = nums[i];
+
+        if(map[target-n] >= 0){
+            return [map[target-n],i]
+        }
+        else {
+            map[n] = i;   //use map to store the value and index position
+        }
+    }
+};
+// console.log(twoSum([2,7,11,15], 9));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
