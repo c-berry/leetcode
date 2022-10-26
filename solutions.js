307a972e672c1277f91186b1fcc8a41fe341947c
Christopher Berry <christopher.m.berry01@gmail.com>
2022-10-26 16:17:59 -0500

    complete daily algorithm

--- solutions.js
+++ solutions.js
@@ -265,3 +265,31 @@ var twoSum = function(nums, target) {
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
+// Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+// I can be placed before V (5) and X (10) to make 4 and 9.
+// X can be placed before L (50) and C (100) to make 40 and 90.
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+// Given a roman numeral, convert it to an integer.
+var romanToInt = function(s) {
+    // Create an obj with the conversions
+    const obj = {M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1}
+    let result = 0
+
+    // Iterate s and assign each number the value from the obj
+    const splitted = s.split('').map(key => obj[key]);
+
+    // Iterate splitted
+    // Do the subtraction when next num is lower than current num
+    // Otherwise, add current num to the result variable
+    for (let i = 0; i < splitted.length; i++) {
+        splitted[i] < splitted[i + 1] ? result -= splitted[i] : result += splitted[i]
+    }
+
+    return result
+}
+// console.log(romanToInt("LVIII"));
+// console.log(romanToInt("MCMXCIV"));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
