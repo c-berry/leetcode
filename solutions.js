0dada2088280699702c5bffd3876410d8c4b9465
Christopher Berry <christopher.m.berry01@gmail.com>
2022-10-27 18:18:08 -0500

    daily algorith involving a linked-list

--- solutions.js
+++ solutions.js
@@ -293,3 +293,51 @@ var romanToInt = function(s) {
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
+
+// You are given the heads of two sorted linked lists list1 and list2.
+// Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+// Return the head of the merged linked list.
+
+    var mergeTwoListsPseudoCode = function(l1, l2) {
+    // initialize a dummy head node
+
+    // initialize a crtNode variable to keep track of the current node, starting with the dummy head node
+
+    // while there are still nodes to compare in two lists
+    // if value of 2nd node is less than value of 1st node
+    // set the current node's link to l2 node
+    // set the l2 node to l2's next node
+    // else
+    // set the current node's link to l1 node
+    // set the l1 node to l1's next node
+
+    // move on to the next node
+
+    // if one of the lists no longer have any nodes to compare, point crt's link to the remaining nodes in the other list
+    // if both lists are empty, point crt's link to null
+
+    // return merged linked list
+}
+
+var mergeTwoLists = function(l1, l2) {
+    var mergedHead = { val : -1, next : null },
+        crt = mergedHead;
+    while(l1 && l2) {
+        if(l1.val > l2.val) {
+            crt.next = l2;
+            l2 = l2.next;
+        } else {
+            crt.next = l1;
+            l1 = l1.next;
+        }
+        crt = crt.next;
+    }
+    crt.next = l1 || l2;
+
+    return mergedHead.next;
+};
+// console.log(mergeTwoLists([1,2,4]), [1,3,4]);
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
