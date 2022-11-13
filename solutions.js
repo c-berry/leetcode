126c23589a21aa9f74ce3f670f9aeb6244092925
Christopher Berry <christopher.m.berry01@gmail.com>
2022-11-12 23:28:19 -0600

    complete daily leet code algorithm

--- solutions.js
+++ solutions.js
@@ -448,3 +448,36 @@ var merge = function(nums1, m, nums2, n) {
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
+// Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+// Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+// Example 1:
+// Input: p = [1,2,3], q = [1,2,3]
+// Output: true
+// Example 2:
+// Input: p = [1,2], q = [1,null,2]
+// Output: false
+var isSameTree = function(p, q) {
+    return JSON.stringify(p) === JSON.stringify(q);
+};
+// console.log(isSameTree([1, 2, 3], [1, 2, 3]));
+// console.log(isSameTree([1, 2], [1, null, 2]));
+
+var isSameTree2 = function(p, q) {
+    if (p == null && q == null){
+        return true
+    }
+    if (p==null || q==null) {
+        return false
+    }
+    if (p.val !== q.val) {
+        return false
+    }
+    return isSameTree2(p.left, q.left) && isSameTree2(p.right,q.right);
+};
+// console.log(isSameTree2([1, 2, 3], [1, 2, 3]));
+// console.log(isSameTree2([1, 2], [1, null, 2]));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+
