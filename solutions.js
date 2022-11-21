47ae99c50089816f741eba1c09141eeabfba90c4
Christopher Berry <christopher.m.berry01@gmail.com>
2022-11-21 17:25:14 -0600

    complete daily algorithm using XOR operator

--- solutions.js
+++ solutions.js
@@ -504,3 +504,18 @@ var isPalindrome2 = function(s) {
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+ // You must implement a solution with a linear runtime complexity and use only constant extra space.
+const singleNumber = (nums) => {
+    let res = 0;
+
+    nums.forEach(num => {res ^= num;});
+
+    return res;
+};
+// console.log(singleNumber([2,2,1]));
+// console.log(singleNumber([4,1,2,1,2]));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
