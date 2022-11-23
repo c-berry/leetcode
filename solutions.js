1224502ee69f2409b198c9ff54955905080451c3
Christopher Berry <christopher.m.berry01@gmail.com>
2022-11-22 18:37:09 -0600

    complete daily algorithm - Pascal's Triangle

--- solutions.js
+++ solutions.js
@@ -519,3 +519,35 @@ const singleNumber = (nums) => {
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
+// Given an integer numRows, return the first numRows of Pascal's triangle.
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+// Example 1:
+// Input: numRows = 5
+// Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+// Example 2:
+// Input: numRows = 1
+// Output: [[1]]
+var generate = function(numRows) {
+    const output = [];
+
+    for (let i = 0; i < numRows; i++) {
+        const rowValue = [];
+
+        for (let j = 0; j < i + 1; j++) {
+            if (j === 0 || j === i) {
+                rowValue[j] = 1;
+            } else {
+                rowValue[j] = output[i - 1][j - 1] + output[i - 1][j];
+            }
+        }
+        output.push(rowValue)
+    }
+
+    return output;
+};
+// console.log(generate(5));
+// console.log(generate(1));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
