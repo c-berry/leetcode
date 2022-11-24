8936954a6c73ee42f7d927b21da5a95b4f5bb92b
Christopher Berry <christopher.m.berry01@gmail.com>
2022-11-23 18:07:52 -0600

    complete daily algorithm

--- solutions.js
+++ solutions.js
@@ -551,3 +551,22 @@ var generate = function(numRows) {
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
+// Given two binary strings a and b, return their sum as a binary string.
+// Example 1:
+// Input: a = "11", b = "1"
+// Output: "100"
+// Example 2:
+// Input: a = "1010", b = "1011"
+// Output: "10101"
+var addBinary = function(a, b) {
+    const aBin = `0b${a}`
+    const bBin = `0b${b}`
+    const sum = BigInt(aBin) + BigInt(bBin)
+    return sum.toString(2)
+};
+// console.log(addBinary("11", "1"));
+// console.log(addBinary("1010", "1011"));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
