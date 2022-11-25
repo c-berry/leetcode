f352e5bf9c1ebc136abf2b40e32aacfa9e8300eb
Christopher Berry <christopher.m.berry01@gmail.com>
2022-11-25 14:42:52 -0600

    daily algorithm involving binry tree

--- solutions.js
+++ solutions.js
@@ -570,3 +570,24 @@ var addBinary = function(a, b) {
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
+// Given the root of a binary tree, return its maximum depth.
+// A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+// Example 1:
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 3
+var maxDepth = function(root) {
+    // Base case...
+    // If the subtree is empty i.e. root is NULL, return depth as 0...
+    if(root == null)  return 0;
+    // if root is not NULL, call the same function recursively for its left child and right child...
+    let leftDepth = maxDepth(root.left);
+    let rightDepth = maxDepth(root.right);
+    // When the two child function return its depth...
+    // Pick the maximum out of these two subtrees and return this value after adding 1 to it...
+    return Math.max(leftDepth, rightDepth) + 1;    // Adding 1 is the current node which is the parent of the two subtrees...
+};
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+
