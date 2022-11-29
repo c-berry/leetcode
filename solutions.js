5b60b5bf98c595b1f6ad1e11404c5171aaad637e
Christopher Berry <christopher.m.berry01@gmail.com>
2022-11-29 14:03:22 -0600

    daily algorithm using Set, map, while, reduce

--- solutions.js
+++ solutions.js
@@ -602,19 +602,54 @@ var maxDepth = function(root) {
 
 var maxProfit = function(prices) {
     // set to first element in array by default
-    let minprice = prices[0];
+    let minPrice = prices[0];
     let profit = 0;
     for (let i = 0; i < prices.length; i++) {
-        if (prices[i] < minprice) {
-            minprice = prices[i];
-        } else if (prices[i] - minprice > profit) {
-            profit = prices[i] - minprice;
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > profit) {
+            profit = prices[i] - minPrice;
         }
     }
     return profit;
 };
 // console.log(maxProfit([7,1,5,3,6,4]));
 // console.log(maxProfit([7,6,4,3,1]));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+// Write an algorithm to determine if a number n is happy.
+//  A happy number is a number defined by the following process:
+//  Starting with any positive integer, replace the number by the sum of the squares of its digits.
+//   Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+// Those numbers for which this process ends in 1 are happy.
+//   Return true if n is a happy number, and false if not.
+//   Example 1:
+//
+// Input: n = 19
+// Output: true
+// Explanation:
+//     12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
+var isHappy = function (n) {
+    let mySet = new Set();
+    let counter = 0;
+    while (counter !== 1 && !mySet.has(n)) {
+        mySet.add(n);
+        let element = n.toString().split("").map((number) => parseInt(number));
+        counter = element.reduce((acc, item) => {
+            acc += Math.pow(item, 2);
+            return acc;
+        }, 0)
+        n = counter;
+    }
+    return counter === 1;
+};
+// console.log(isHappy(19));
+// console.log(isHappy(2));
 
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
