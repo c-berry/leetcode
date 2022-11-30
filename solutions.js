e3ca04395035166dd6b9e9012c3f0b3f3b9cc776
Christopher Berry <christopher.m.berry01@gmail.com>
2022-11-30 12:36:07 -0600

    daily alrogithm using recursion, binary tree

--- solutions.js
+++ solutions.js
@@ -654,3 +654,28 @@ var isHappy = function (n) {
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
+// Given the root of a binary tree, return the preorder traversal of its nodes' values.
+// Example 1:
+// Input: root = [1,null,2,3]
+// Output: [1,2,3]
+// Example 2:
+// Input: root = []
+// Output: []
+var preorderTraversal = function (root) {
+    const ans = [];
+    preorder(root);
+    return ans;
+
+    function preorder(root) {
+        if (root === null) {
+            return;
+        }
+        ans.push(root.val);
+        preorder(root.left);
+        preorder(root.right);
+    }
+};
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
