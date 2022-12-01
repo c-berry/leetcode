896f6fe5f21587e181fe142131990e58bd310e67
Christopher Berry <christopher.m.berry01@gmail.com>
2022-12-01 12:16:11 -0600

    daily algorithm using HashMap

--- solutions.js
+++ solutions.js
@@ -679,3 +679,29 @@ var preorderTraversal = function (root) {
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
+// Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+//   Example 1:
+// Input: nums = [1,2,3,1], k = 3
+// Output: true
+// Example 2:
+// Input: nums = [1,0,1,1], k = 1
+// Output: true
+var containsNearbyDuplicate = function(nums, k) {
+    var map = new Map();
+    for(var i = 0; i < nums.length; i++){
+        if(map.get(nums[i]) !== undefined){
+            if(i - map.get(nums[i]) <= k){
+                return true;
+            }
+            else map.set(nums[i], i);
+        }
+        else map.set(nums[i], i);
+    }
+    return false;
+};
+// console.log(containsNearbyDuplicate([1,2,3,1]));
+// console.log(containsNearbyDuplicate([1,0,1,1]));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
