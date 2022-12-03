81a3d4560437eacd45c606e0b1c14afc0fcdeab6
Christopher Berry <christopher.m.berry01@gmail.com>
2022-12-02 20:22:26 -0600

    daily leetcode algorithm

--- solutions.js
+++ solutions.js
@@ -705,3 +705,30 @@ var containsNearbyDuplicate = function(nums, k) {
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
+// Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+//
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+// Example 1:
+// Input: rowIndex = 3
+// Output: [1,3,3,1]
+// Example 2:
+// Input: rowIndex = 0
+// Output: [1]
+var getRow = function(rowIndex) {
+    var result = [1];
+    for(var i = 0; i < rowIndex; i++){
+        result.unshift(1);
+        for(var j = 0; j <= i; j++){
+            result[j] = (result[j + 1]||0) + (result[j + 2]||0);
+        }
+        result.pop();
+        result.unshift(1);
+    }
+    return result;
+};
+console.log(getRow(3));
+console.log(getRow(0));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
