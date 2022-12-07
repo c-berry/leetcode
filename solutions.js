3aac9da233bfc21c31acae01f7c34deb3fa39bba
Christopher Berry <christopher.m.berry01@gmail.com>
2022-12-06 19:36:33 -0600

    daily leetcode algorithm

--- solutions.js
+++ solutions.js
@@ -732,3 +732,25 @@ console.log(getRow(0));
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
+// Given an array nums of size n, return the majority element.
+//  The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+//   Example 1:
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+var majorityElement = function(nums) {
+    var obj = {};
+
+    for(var i = 0; i < nums.length; i++){
+        obj[nums[i]] = obj[nums[i]] + 1 || 1;
+        if(obj[nums[i]] > nums.length / 2)  return nums[i];
+    }
+};
+// console.log(majorityElement([3,2,3]));
+// console.log(majorityElement([2,2,1,1,1,2,2]));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
