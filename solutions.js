d7da0e947a69ebcd26cf8b03308b6adacf35844d
Christopher Berry <christopher.m.berry01@gmail.com>
2022-12-08 18:26:55 -0600

    daily leetcode algorithm involving binary tree

--- solutions.js
+++ solutions.js
@@ -754,3 +754,17 @@ var majorityElement = function(nums) {
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
+// Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+var sortedArrayToBST = function (nums) {
+    if (nums.length === 0) {
+        return null;
+    }
+    let middle = Math.floor(nums.length / 2);
+    let root = new TreeNode(nums[middle]);
+    root.left = sortedArrayToBST(nums.slice(0, middle));
+    root.right = sortedArrayToBST(nums.slice(middle + 1));
+    return root;
+};
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
