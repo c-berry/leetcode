55a0beff0bb1aa0dd1a8b9875e33c76712a2ea2c
Christopher Berry <christopher.m.berry01@gmail.com>
2022-12-09 18:02:06 -0600

    daily leetcode algorithm traversing binary tree

--- solutions.js
+++ solutions.js
@@ -768,3 +768,27 @@ var sortedArrayToBST = function (nums) {
 
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+// Given the root of a binary tree, return the postorder traversal of its nodes' values.
+// Example 1:
+// Input: root = [1,null,2,3]
+// Output: [3,2,1]
+var postorderTraversal = function(root) {
+    if (!root) return [];
+
+    var result = [], stack = [root];
+
+    while (stack.length) {
+        var node = stack.pop();
+        // insert the node val to the front
+        result.unshift(node.val);
+        if (node.left) stack.push(node.left); // left first
+        if (node.right) stack.push(node.right); // then right
+    }
+    return result;
+};
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+
