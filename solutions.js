64ee04fb84e5432b402fbaf9377fb3decd5da4c7
Christopher Berry <christopher.m.berry01@gmail.com>
2022-12-12 22:43:23 -0600

    daily algorithm involving binary trww

--- solutions.js
+++ solutions.js
@@ -791,4 +791,33 @@ var postorderTraversal = function(root) {
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
+// Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+// Example 1:
+// Input: root = [1,2,2,3,4,4,3]
+// Output: true
+// Example 2:
+// Input: root = [1,2,2,null,3,null,3]
+// Output: false
+var isSymmetric = function(root) {
+    if (!root) { // Sanity check
+        return true;
+    }
 
+    // Check if tree s & t are mirroring each other
+    function isMirror(s, t) {
+        if (!s && !t) {
+            return true; // Both nodes are null, ok
+        }
+        if (!s || !t || s.val !== t.val) {
+            return false; // Found a mismatch
+        }
+        // Compare the left subtree of `s` with the right subtree of `t`
+        // and the right subtree of `s` with the left subtree of `t`
+        return isMirror(s.left, t.right) && isMirror(s.right, t.left);
+    }
+
+    return isMirror(root.left, root.right);
+};
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
