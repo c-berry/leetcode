5c5ea75304c1ca1df5d531331c428c632d0e199a
Christopher Berry <christopher.m.berry01@gmail.com>
2022-12-14 21:15:15 -0600

    daily algorithm traversing binary tree

--- solutions.js
+++ solutions.js
@@ -821,3 +821,21 @@ var isSymmetric = function(root) {
 
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+// Given the root of a binary tree, invert the tree, and return its root.
+//   Example 1:
+// Input: root = [4,2,7,1,3,6,9]
+// Output: [4,7,2,9,6,3,1]
+// Example 2:
+// Input: root = [2,1,3]
+// Output: [2,3,1]
+function invertTree(root) {
+    if (root) {
+        [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
+    }
+    return root;
+}
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
