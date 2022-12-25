588d5b7f0145a4bc9018576eeb45804797fbb9c0
Christopher Berry <christopher.m.berry01@gmail.com>
2022-12-24 19:09:58 -0600

    daily algorithm via leetcode

--- solutions.js
+++ solutions.js
@@ -879,5 +879,16 @@ var findTheDifference = function(s, t) {
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
+// Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+var findDisappearedNumbers = function(nums) {
+    var res = [];
+    for(var i = 1; i < nums.length+1; i++){
+        if(nums.indexOf(i) === -1)res.push(i)
+    }
+    return res;
+};
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
 
