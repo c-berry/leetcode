1b765eb9677bafd46139037261641a3b34a6ab80
Christopher Berry <christopher.m.berry01@gmail.com>
2022-12-28 22:35:51 -0600

    linked list algorithm via leetcode

--- solutions.js
+++ solutions.js
@@ -930,3 +930,22 @@ var inorderTraversal = function(root) {
 
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+//     Example 1:
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+let reverseList = function(head) {
+    let prev = null, next = null;
+    while (head) {
+        next = head.next
+        head.next = prev;
+        prev = head;
+        head = next;
+    }
+    return prev;
+};
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
