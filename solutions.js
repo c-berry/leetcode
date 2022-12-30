d0192ee799211008b41d688af1ccfbcc94bae3a6
Christopher Berry <christopher.m.berry01@gmail.com>
2022-12-29 23:40:42 -0600

    merging two linked list algorithm via leetcode

--- solutions.js
+++ solutions.js
@@ -935,7 +935,7 @@ var inorderTraversal = function(root) {
 //     Example 1:
 // Input: head = [1,2,3,4,5]
 // Output: [5,4,3,2,1]
-let reverseList = function(head) {
+var reverseList = function(head) {
     let prev = null, next = null;
     while (head) {
         next = head.next
@@ -945,6 +945,31 @@ let reverseList = function(head) {
     }
     return prev;
 };
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+// Imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not. You need to merge the two trees into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of the new tree.
+// Return the merged tree.
+//     Note: The merging process must start from the root nodes of both trees.
+//     Example 1:
+// Input: root1 = [1,3,2,5], root2 = [2,1,3,null,4,null,7]
+// Output: [3,4,5,5,4,null,7]
+
+var mergeTrees = function(t1, t2) {
+    if (!t1) {
+        return t2;
+    }
+    if (!t2) {
+        return t1;
+    }
+    t1.val += t2.val;
+
+    t1.left = mergeTrees(t1.left, t2.left);
+    t1.right = mergeTrees(t1.right, t2.right);
+
+    return t1;
+}
 
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
